cc3f5186328131a125cad89ce8115606e823b95e
drashtiv07-coder <drashti.variyam07@gmail.com>
2026-01-25 12:51:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let snake = [{ x: 200, y: 200 }];
+let direction = "RIGHT";
+let food = { x: 100, y: 100 };
+let size = 20;
+
+// 🎮 GAME LOOP
+function gameLoop() {
+  moveSnake();
+  drawGame();
+}
+setInterval(gameLoop, 200);
+
+function moveSnake() {
+  let head = { ...snake[0] };
+
+  if (direction === "LEFT") head.x -= size;
+  if (direction === "RIGHT") head.x += size;
+  if (direction === "UP") head.y -= size;
+  if (direction === "DOWN") head.y += size;
+
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    food = {
+      x: Math.floor(Math.random() * 20) * size,
+      y: Math.floor(Math.random() * 20) * size
+    };
+  } else {
+    snake.pop();
+  }
+}
+
+function drawGame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Snake
+  ctx.fillStyle = "lime";
+  snake.forEach(s => ctx.fillRect(s.x, s.y, size, size));
+
+  // Food
+  ctx.fillStyle = "red";
+  ctx.fillRect(food.x, food.y, size, size);
+}
+
+// 📷 CAMERA + HAND GESTURES
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+  video.srcObject = stream;
+});
+
+const hands = new Hands({
+  locateFile: file =>
+    `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+});
+
+hands.setOptions({
+  maxNumHands: 1,
+  minDetectionConfidence: 0.7,
+  minTrackingConfidence: 0.7
+});
+
+hands.onResults(results => {
+  if (!results.multiHandLandmarks) return;
+
+  const hand = results.multiHandLandmarks[0];
+  const x = hand[0].x;
+  const y = hand[0].y;
+
+  if (x < 0.3) direction = "LEFT";
+  else if (x > 0.7) direction = "RIGHT";
+  else if (y < 0.3) direction = "UP";
+  else if (y > 0.7) direction = "DOWN";
+});
+
+const camera = new Camera(video, {
+  onFrame: async () => {
+    await hands.send({ image: video });
+  },
+  width: 640,
+  height: 480
+});
+camera.start();
